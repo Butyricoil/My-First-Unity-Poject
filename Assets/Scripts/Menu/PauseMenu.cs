2568836f1d1ffebb2098ee9da3aa8b2cfa09bd5c
Butyricoil <butyricoil@gmail.com>
2025-03-15 21:16:04 +0400

    Menu Fix

--- Assets/Scripts/Menu/PauseMenu.cs
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -11,19 +11,20 @@ public class PauseMenu : MonoBehaviour
         pauseMenu.SetActive(!isActive);
 
         if (!isActive)
-            Time.timeScale = 0f;
+            Time.timeScale = 0f; // Пауза
         else
-            Time.timeScale = 1f;
+            Time.timeScale = 1f; // Возобновление
     }
 
     public void ResumeGame()
     {
         pauseMenu.SetActive(false);
-        Time.timeScale = 1f;
+        Time.timeScale = 1f; // Возобновление игры
     }
 
     public void BackToMainMenu()
     {
+        Time.timeScale = 1f; // Сбрасываем паузу перед загрузкой сцены
         SceneManager.LoadScene("MainMenu");
     }
 }
