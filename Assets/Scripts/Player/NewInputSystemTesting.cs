137ffb9a18b30b180dd4122cf4b91f7d1af6437b
Butyricoil <butyricoil@gmail.com>
2025-03-17 23:28:37 +0400

    NEW INPUT SYSTEM test

--- Assets/Scripts/Player/NewInputSystemTesting.cs
+++ Assets/Scripts/Player/NewInputSystemTesting.cs
@@ -5,10 +5,22 @@ using UnityEngine.InputSystem;
 public class NewInputSystemTesting : MonoBehaviour
 {
     private Rigidbody _rigidbody;
+    private PlayerInput _playerInput;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _playerInput = GetComponent<PlayerInput>();
+
+        PlayerInputActions playerInputActions = new PlayerInputActions;
+        playerInputActions.Player.Enable();
+        playerInputActions.Player.Jump.performed += Jump;
+        playerInputActions.Player.Movement.performed += Movement_performed;
+    }
+
+    private void Movement_performed(InputAction.CallbackContext context)
+    {
+        Debug.Log(context);
     }
 
     public void Jump(InputAction.CallbackContext context)
