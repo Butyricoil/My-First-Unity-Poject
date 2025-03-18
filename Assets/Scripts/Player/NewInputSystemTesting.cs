5db283e6cd7f4e909fa0cb9ada6b1da768c20e29
Butyricoil <butyricoil@gmail.com>
2025-03-18 21:04:01 +0400

    input test system WORKING

--- Assets/Scripts/Player/NewInputSystemTesting.cs
+++ Assets/Scripts/Player/NewInputSystemTesting.cs
@@ -5,31 +5,55 @@ using UnityEngine.InputSystem;
 public class NewInputSystemTesting : MonoBehaviour
 {
     private Rigidbody _rigidbody;
-    private PlayerInput _playerInput;
+    private PlayerInputActions _playerInputActions; // Сохраняем экземпляр Input Actions
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
-        _playerInput = GetComponent<PlayerInput>();
 
-        PlayerInputActions playerInputActions = new PlayerInputActions();
-        playerInputActions.Player.Enable();
-        playerInputActions.Player.Jump.performed += Jump;
-        playerInputActions.Player.Movement.performed += Movement_performed;
+        // Инициализация системы ввода
+        _playerInputActions = new PlayerInputActions();
+        _playerInputActions.Player.Enable();
+
+        // Подписка на события
+        _playerInputActions.Player.Jump.performed += Jump;
+        _playerInputActions.Player.Movement.performed += Movement_performed;
+    }
+
+    private void Update()
+    {
+        Vector2 inputVector =  _playerInputActions.Player.Movement.ReadValue<Vector2>();
+        float _speed = 5f;
+        _rigidbody.AddForce(new Vector3(inputVector.x, 0, inputVector.y) * _speed, ForceMode.Force);
     }
 
     private void Movement_performed(InputAction.CallbackContext context)
     {
         Debug.Log(context);
+        // Здесь можно добавить логику для движения
+        Vector2 inputVector = context.ReadValue<Vector2>();
+        float _speed = 5f;
+        _rigidbody.AddForce(new Vector3(inputVector.x, 0, inputVector.y) * _speed, ForceMode.Force);
     }
 
-    public void Jump(InputAction.CallbackContext context)
+    private void Jump(InputAction.CallbackContext context)
     {
+        Debug.Log(context);
         if (context.performed)
         {
             Debug.Log("Jump" + context.phase);
-            _rigidbody.AddForce(Vector3.up *5f, ForceMode.Impulse);
+            _rigidbody.AddForce(Vector3.up * 5f, ForceMode.Impulse);
         }
     }
 
+    private void OnDestroy()
+    {
+        // Отписываемся от событий и отключаем систему ввода
+        if (_playerInputActions != null)
+        {
+            _playerInputActions.Player.Jump.performed -= Jump;
+            _playerInputActions.Player.Movement.performed -= Movement_performed;
+            _playerInputActions.Player.Disable();
+        }
+    }
 }
