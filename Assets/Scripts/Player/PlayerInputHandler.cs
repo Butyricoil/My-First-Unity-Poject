dee00d079ba5430a01ab780a1d8f9678330ae213
Butyricoil <butyricoil@gmail.com>
2025-03-19 18:46:08 +0400

    Touchscreen fix

--- Assets/Scripts/Player/PlayerInputHandler.cs
+++ Assets/Scripts/Player/PlayerInputHandler.cs
@@ -4,7 +4,8 @@ using UnityEngine.InputSystem;
 public class PlayerInputHandler : MonoBehaviour
 {
     public Vector2 MovementInput { get; private set; }
-    public bool JumpTriggered { get; private set; }
+    public bool IsJumpTriggered { get; private set; }
+    public bool IsMoving { get; private set; } // Новый флаг для отслеживания движения
 
     private PlayerInputActions _playerInputActions;
 
@@ -15,29 +16,36 @@ public class PlayerInputHandler : MonoBehaviour
 
         _playerInputActions.Player.Jump.performed += OnJump;
         _playerInputActions.Player.Movement.performed += OnMovement;
-        _playerInputActions.Player.Movement.canceled += OnMovement;
+        _playerInputActions.Player.Movement.canceled += OnMovementCanceled;
     }
 
     private void OnJump(InputAction.CallbackContext context)
     {
-        JumpTriggered = context.performed;
+        IsJumpTriggered = context.performed;
     }
 
     private void OnMovement(InputAction.CallbackContext context)
     {
         MovementInput = context.ReadValue<Vector2>();
+        IsMoving = true; // Устанавливаем флаг движения
+    }
+
+    private void OnMovementCanceled(InputAction.CallbackContext context)
+    {
+        MovementInput = Vector2.zero;
+        IsMoving = false; // Сбрасываем флаг движения
     }
 
     public void ResetJumpTrigger()
     {
-        JumpTriggered = false;
+        IsJumpTriggered = false;
     }
 
     private void OnDestroy()
     {
         _playerInputActions.Player.Jump.performed -= OnJump;
         _playerInputActions.Player.Movement.performed -= OnMovement;
-        _playerInputActions.Player.Movement.canceled -= OnMovement;
+        _playerInputActions.Player.Movement.canceled -= OnMovementCanceled;
         _playerInputActions.Player.Disable();
     }
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -22,13 +22,21 @@ public class PlayerMovement : MonoBehaviour
 
     private void Move()
     {
-        Vector2 inputVector = _inputHandler.MovementInput;
-        _rigidbody.AddForce(new Vector3(inputVector.x, 0, inputVector.y) * _speed, ForceMode.Force);
+        if (_inputHandler.IsMoving) // Проверяем, активен ли ввод
+        {
+            Vector2 inputVector = _inputHandler.MovementInput;
+            Vector3 movement = new Vector3(inputVector.x, 0, inputVector.y) * _speed;
+            _rigidbody.linearVelocity = new Vector3(movement.x, _rigidbody.linearVelocity.y, movement.z);
+        }
+        else
+        {
+            _rigidbody.linearVelocity = new Vector3(0, _rigidbody.linearVelocity.y, 0); // Останавливаем движение
+        }
     }
 
     private void Jump()
     {
-        if (_inputHandler.JumpTriggered)
+        if (_inputHandler.IsJumpTriggered)
         {
             _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
             _inputHandler.ResetJumpTrigger(); // Сбрасываем триггер после прыжка
